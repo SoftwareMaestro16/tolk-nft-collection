2d0082c818839f767ff25102bdc8174bf161890e
SoftwareMaestro16 <softwaremaestro16@gmail.com>
2025-07-08 17:53:02 +0300

    add

--- scripts/deployNftCollectionContract.ts
+++ scripts/deployNftCollectionContract.ts
@@ -1,9 +1,54 @@
-import { toNano } from '@ton/core';
+import { Address, beginCell, Cell, toNano } from '@ton/core';
 import { NFTCollection } from '../wrappers/NftCollectionContract';
 import { compile, NetworkProvider } from '@ton/blueprint';
+import { NFTMetadata } from '../utils/constants';
 
 export async function run(provider: NetworkProvider) {
-    const nftCollectionContract = provider.open(NFTCollection.createFromConfig({}, await compile('NftCollectionContract')));
+    const encodeRoyaltyParams = (params: { royaltyFactor: number, royaltyBase: number, royaltyAddress: Address}): Cell => {
+        return beginCell()
+            .storeUint(params.royaltyFactor, 16)
+            .storeUint(params.royaltyBase, 16)
+            .storeAddress(params.royaltyAddress)
+        .endCell()
+    }
+
+    const encodeMetadataNft = (content: string): Cell => {
+        return beginCell()
+            .storeStringTail(content)
+        .endCell()
+    }
+
+    const royaltyParams = {
+        royaltyFactor: 3, 
+        royaltyBase: 100,
+        royaltyAddress: Address.parse("0QCsMm47egxSofgw5Y-l34ZeMw6vPYUUyTIjYT3HTafpmH9O")
+        // royaltyAddress: provider.sender().address as Address
+    }
+
+    const royaltyParamsCell = encodeRoyaltyParams(royaltyParams);
+
+    const commonContent2 = beginCell()
+        .storeStringTail("")
+    .endCell()
+
+    const commonContent1 = beginCell()
+        .storeUint(1, 8)
+        .storeStringTail(NFTMetadata.collectionMetadata)
+    .endCell()
+
+    const collectionContent = beginCell()
+        .storeUint(1, 8)
+        .storeRef(commonContent1)
+        .storeRef(commonContent2)
+    .endCell()
+
+    const nftCollectionContract = provider.open(NFTCollection.createFromConfig({
+        ownerAddress: provider.sender().address as Address,
+        nextItemIndex: 0,
+        content: collectionContent,
+        nftItemCode: await compile('NFTItem'),
+        royaltyParams: royaltyParamsCell,
+    }, await compile('NFTCollection')));
 
     await nftCollectionContract.sendDeploy(provider.sender(), toNano('0.05'));
 
---
+++ utils/constants.ts
@@ -0,0 +1,16 @@
+export class NFTMetadata {
+    static collectionMetadata: string = "https://softwaremaestro16.github.io/nft_collection/folder_for_mintory/nft_collection.json";
+  
+    static nftMetadata: string[] = [
+      "https://softwaremaestro16.github.io/nft_collection/folder_for_mintory/nft_meta_1.json",
+      "https://softwaremaestro16.github.io/nft_collection/folder_for_mintory/nft_meta_2.json",
+      "https://softwaremaestro16.github.io/nft_collection/folder_for_mintory/nft_meta_3.json",
+      "https://softwaremaestro16.github.io/nft_collection/folder_for_mintory/nft_meta_4.json",
+      "https://softwaremaestro16.github.io/nft_collection/folder_for_mintory/nft_meta_5.json",
+    ];
+};
+
+export class JettonData {
+    static jettonMinterAddress: string = "kQBpqkbPrhSjleAQ8W9TJpZBj6K3GKijCH-Uz_6H7UnaqVTI";
+    static jettonWalletCode: string = "te6cckECEQEAAyMAART/APSkE/S88sgLAQIBYgIQAgLMAwYCAdQEBQDDCDHAJJfBOAB0NMDAXGwlRNfA/AM4PpA+kAx+gAxcdch+gAx+gAwc6m0AALTH4IQD4p+pVIgupUxNFnwCeCCEBeNRRlSILqWMUREA/AK4DWCEFlfB7y6k1nwC+BfBIQP8vCAAET6RDBwuvLhTYAIBIAcPAgEgCAoB8VA9M/+gD6QCHwAe1E0PoA+kD6QNQwUTahUirHBfLiwSjC//LiwlQ0QnBUIBNUFAPIUAT6AljPFgHPFszJIsjLARL0APQAywDJIPkAcHTIywLKB8v/ydAE+kD0BDH6ACDXScIA8uLEd4AYyMsFUAjPFnD6AhfLaxPMgJAJ6CEBeNRRnIyx8Zyz9QB/oCIs8WUAbPFiX6AlADzxbJUAXMI5FykXHiUAioE6CCCcnDgKAUvPLixQTJgED7ABAjyFAE+gJYzxYBzxbMye1UAgEgCw4C9ztRND6APpA+kDUMAjTP/oAUVGgBfpA+kBTW8cFVHNtcFQgE1QUA8hQBPoCWM8WAc8WzMkiyMsBEvQA9ADLAMn5AHB0yMsCygfL/8nQUA3HBRyx8uLDCvoAUaihggiYloBmtgihggiYloCgGKEnlxBJEDg3XwTjDSXXCwGAMDQBwUnmgGKGCEHNi0JzIyx9SMMs/WPoCUAfPFlAHzxbJcYAQyMsFJM8WUAb6AhXLahTMyXH7ABAkECMAfMMAI8IAsI4hghDVMnbbcIAQyMsFUAjPFlAE+gIWy2oSyx8Syz/JcvsAkzVsIeIDyFAE+gJYzxYBzxbMye1UANc7UTQ+gD6QPpA1DAH0z/6APpAMFFRoVJJxwXy4sEnwv/y4sIFggkxLQCgFrzy4sOCEHvdl97Iyx8Vyz9QA/oCIs8WAc8WyXGAGMjLBSTPFnD6AstqzMmAQPsAQBPIUAT6AljPFgHPFszJ7VSAAg9QBBrkPaiaH0AfSB9IGoYAmmPwQgLxqKMqRBdQQg97svvCd0JWPlxYumfmP0AGAnQKBHkKAJ9ASxniwDni2Zk9qpAAboPYF2omh9AH0gfSBqGG7XEd1";
+} 
